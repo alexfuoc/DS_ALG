8ab16906b3edcaea7c41855df0faee84a19fcff9
Alex Fuoco <alexfuoc@gmail.com>
2021-09-22 14:26:32 -0400

    Initializing Project

---
+++ BigO/FindNemo.py
@@ -0,0 +1,11 @@
+
+def find_nemo(cast):
+    for name in cast:
+        if name == 'nemo':
+            print('Found Nemo!')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    characters = ['nemo']
+    find_nemo(characters)
